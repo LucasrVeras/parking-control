6f1503c49a625d2f813141db4774c91738b673ae
Lucas Veras <lucas.veras01@gmail.com>
2022-07-19 19:04:26 -0300

    Save and list implemented

--- src/main/java/br/com/api/parkingcontrol/controllers/ParkingSpotController.java
+++ src/main/java/br/com/api/parkingcontrol/controllers/ParkingSpotController.java
@@ -6,10 +6,15 @@ import java.time.ZoneId;
 import javax.validation.Valid;
 
 import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,16 +33,46 @@ public class ParkingSpotController {
 	final ParkingSpotService parkingSpotService;
 	
 	@PostMapping
-	public ResponseEntity<Object> saveParkingSpot(@RequestBody @Valid 
+	public ResponseEntity<Object> saveParkingSpot(@RequestBody @Valid
 			ParkingSpotDto parkingSpotDto){
-			
+		
+		 if(parkingSpotService
+				 .existsByLicensePlateCar(parkingSpotDto.getLicensePlateCar())){
+	            return ResponseEntity
+	            		.status(HttpStatus.CONFLICT)
+	            		.body("Conflict: License Plate Car is already in use!");
+	        }
+	        if(parkingSpotService.existsByParkingSpotNumber(parkingSpotDto.getParkingSpotNumber())){
+	            return ResponseEntity
+	            		.status(HttpStatus.CONFLICT)
+	            		.body("Conflict: Parking Spot is already in use!");
+	        }
+	        if(parkingSpotService
+	        		.existsByApartmentAndBlock(parkingSpotDto.getApartment(), parkingSpotDto.getBlock())){
+	            return ResponseEntity
+	            		.status(HttpStatus.CONFLICT)
+	            		.body("Conflict: Parking Spot already registered for this apartment/block!");
+	        }
+		
 		var parkingSpotModel = new ParkingSpotModel();
 		BeanUtils.copyProperties(parkingSpotDto, parkingSpotModel);
 		parkingSpotModel.setRegistrationDate(LocalDateTime.now(ZoneId.of("UTC")));
 		
 		return ResponseEntity.status(HttpStatus.CREATED)
 				.body(parkingSpotService.save(parkingSpotModel));
+	}
+	
+	//Busca paginada
+	@GetMapping
+	public ResponseEntity<Page<ParkingSpotModel>> getAllParkingSpots(
+			@PageableDefault(page = 0, 
+			size = 10, 
+			sort = "id", 
+			direction = Sort.Direction.ASC) Pageable pageable){
 		
+		return ResponseEntity.status(HttpStatus.OK)
+				.body(parkingSpotService
+				.findAll(pageable));
 	}
 	
 }
--- src/main/java/br/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/br/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -1,5 +1,9 @@
 package br.com.api.parkingcontrol.services;
 
+import javax.transaction.Transactional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import br.com.api.parkingcontrol.models.ParkingSpotModel;
@@ -13,8 +17,28 @@ public class ParkingSpotService {
 	final ParkingSpotRepository parkingSpotRepository;
 	
 	// Save cadastro carro
+	@Transactional
 	public Object save(ParkingSpotModel parkingSpotModel) {
+		return parkingSpotRepository.save(parkingSpotModel);
+	}
+
+	public boolean existsByLicensePlateCar(String licensePlateCar) {
 		
-		return null;
+		return parkingSpotRepository
+				.existsByLicensePlateCar(licensePlateCar);
+	}
+
+	public boolean existsByParkingSpotNumber(String parkingSpotNumber) {
+		return parkingSpotRepository
+				.existsByParkingSpotNumber(parkingSpotNumber);
+	}
+
+	public boolean existsByApartmentAndBlock(String apartment, String block) {
+		return parkingSpotRepository
+				.existsByApartmentAndBlock(apartment, block);
+	}
+
+	public Page<ParkingSpotModel> findAll(Pageable pageable) {
+		return parkingSpotRepository.findAll(pageable);
 	}
 }
